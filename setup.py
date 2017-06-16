cff8d1301d4ba6d10e930a9c27112cfbd340a50c
Dillon Barker <dor.barker@gmail.com>
2017-06-16 11:46:24 -0600

    Fix setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(
 
         entry_points={
             'console_scripts': [
-                'gcfilter = gcfilter:main'
+                'gcfilter = gcfilter.gcfilter:main'
                 ]}
 )
