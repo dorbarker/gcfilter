8e032b41aa490294aa75d36bb3369cb10662acdd
Dillon Barker <dor.barker@gmail.com>
2017-06-16 11:16:25 -0600

    Add GPL 3+; add setup.py

--- gcfilter.py
+++ gcfilter.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
-"""Filter FASTQ files by GC content.
+#    gcfilter - Filters FASTQ files by GC content
+#    Copyright (C) 2017 Dillon Barker
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""gcfilter - Filters FASTQ files by GC content
+Copyright (C) 2017 Dillon Barker
+program is free software: you can redistribute it and/or modify
+under the terms of the GNU General Public License as published by
+Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
 Usage: gcfilter.py <lower> <upper> <fastq>
 
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+        name='gcfilter',
+        version='0.1',
+        packages=find_packages(),
+        install_requires=['biopython', 'docopt'],
+
+        author='Dillon Barker',
+        author_email='dillon.barker@canada.ca',
+        description='Filters FASTQ files on GC content',
+        license='GPL 3+',
+        keywords='bioinformatics fastq sequence',
+        url='https://github.com/dorbarker/gcfilter',
+
+        entry_points={
+            'console_scripts': [
+                'gcfilter = gcfilter:main'
+                ]}
+)
